a07a6f92e9419d617a8e40ef576b4fa1b1fc49a9
Websterek <websterek@gmail.com>
2026-04-14 14:35:58 +0200

    Log drag-and-drop events for debugging
    Add Debug.WriteLine calls to OnDragEnter and OnDragOver to trace DnD
    data transfer and accepted effects. Also apply a minor formatting tweak
    in Program.cs (brace/newline).

--- Program.cs
+++ Program.cs
@@ -8,8 +8,7 @@ internal class Program
 {
     [STAThread]
     public static void Main(string[] args)
-    {
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    {BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
     public static AppBuilder BuildAvaloniaApp()
