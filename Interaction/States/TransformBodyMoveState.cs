252382ff7dbd6d2ce6a828aa1a9e75cfcf0dd3a3
Websterek <websterek@gmail.com>
2026-04-30 22:57:01 +0200

    feat(interaction): add stub state classes for all remaining gestures

---
+++ Interaction/States/TransformBodyMoveState.cs
@@ -0,0 +1,16 @@
+using Avalonia.Input;
+namespace CGReferenceBoard.Interaction.States;
+
+/// <summary>
+/// TODO: Extract from MainWindow code-behind. Currently a stub.
+/// </summary>
+public sealed class TransformBodyMoveState : IInteractionState
+{
+    public void Enter(IInteractionContext ctx) { }
+    public void Exit(IInteractionContext ctx) { }
+    public StateTransition OnPointerPressed(PointerPressedEventArgs e, IInteractionContext ctx) => StateTransition.Stay;
+    public StateTransition OnPointerMoved(PointerEventArgs e, IInteractionContext ctx) => StateTransition.Stay;
+    public StateTransition OnPointerReleased(PointerReleasedEventArgs e, IInteractionContext ctx) => StateTransition.Pop;
+    public StateTransition OnPointerCaptureLost(PointerCaptureLostEventArgs e, IInteractionContext ctx) => StateTransition.Pop;
+    public StateTransition OnKeyDown(KeyEventArgs e, IInteractionContext ctx) => StateTransition.Stay;
+}
